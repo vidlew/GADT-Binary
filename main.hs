89c7dbdfda6c4493146c6693fecd5c1397d3315b
David <vidlew@outlook.com>
2018-04-15 18:50:36 -0300

    modified:   main.hs

--- main.hs
+++ main.hs
@@ -6,6 +6,6 @@ import System.Random
 m = SI $ SI $ SO $ SI $ SI $ SI SB
 
 main :: IO ()
-main = do n <- getStdRandom (randomR (0,elems m))
+main = do n <- getStdRandom (randomR (0,(elems m)-1))
           print $ toBinOrd n m
 
