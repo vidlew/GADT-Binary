441ceefbd3853269e12a0c7cabc5297ec4989995
David <vidlew@outlook.com>
2018-04-15 19:10:06 -0300

    modified:   main.hs

--- main.hs
+++ main.hs
@@ -9,4 +9,4 @@ main :: IO ()
 --main = do n <- getStdRandom (randomR (0,(elems m)-1))
 --          print $ toBinOrd n m
 --          putStrLn $ (reverse $ show $ toBinOrd n m) >>= \x -> if x `elem` "()" then [] else if x == ' ' then return '.' else return x
-main = let l = (>>= \x -> if x `elem` "()" then "" else if x == ' ' then return '.' else return x) <$> show <$> binOrdList m in putStr $ unlines l
+main = let l = reverse <$> (>>= \x -> if x `elem` "()" then "" else if x == ' ' then return '.' else return x) <$> show <$> binOrdList m in putStr $ unlines l
