45d46c1034e82bdf13aa92af8dabd3cd377dfd10
David <vidlew@outlook.com>
2018-04-15 19:07:42 -0300

    modified:   main.hs

--- main.hs
+++ main.hs
@@ -6,7 +6,7 @@ import System.Random
 m = SI $ SI $ SO $ SI $ SI $ SI SB
 
 main :: IO ()
-main = do n <- getStdRandom (randomR (0,(elems m)-1))
+--main = do n <- getStdRandom (randomR (0,(elems m)-1))
 --          print $ toBinOrd n m
-          putStrLn $ (reverse $ show $ toBinOrd n m) >>= \x -> if x `elem` "()" then [] else if x == ' ' then return '.' else return x
-
+--          putStrLn $ (reverse $ show $ toBinOrd n m) >>= \x -> if x `elem` "()" then [] else if x == ' ' then return '.' else return x
+main = let l = (>>= \x -> if x `elem` "()" then "" else if x == ' ' then return '.' else return x) <$> show <$> binOrdList m in putStr $ unlines l
