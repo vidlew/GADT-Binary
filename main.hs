b4eafcf612f054d95d9c491c501c0a8dda7d669b
David <vidlew@outlook.com>
2018-04-25 17:05:07 -0300

    modified:   main.hs

--- main.hs
+++ main.hs
@@ -11,4 +11,4 @@ main :: IO ()
 --          print $ toBinOrd n m
 --          putStrLn $ (reverse $ show $ toBinOrd n m) >>= \x -> if x `elem` "()" then [] else if x == ' ' then return '.' else return x
 --main = let l = reverse <$> (>>= \x -> if x `elem` "()" then "" else if x == ' ' then return '.' else return x) <$> show <$> binOrdList m in putStr $ unlines l
-main = let l = reverse <$> (>>= \x -> if x `elem` "( C)" then ""  else if x `elem` "RG" then "1" else if x=='L' then "0" else "2") <$> show <$> binOrdList m in putStr $ "Numbers from 0 to " ++ (show $ (elems m)-1) ++ " in binary:\n" ++ unlines l
+main = let l = reverse <$> (>>= \x -> if x `elem` "( C)" then ""  else if x `elem` "RG" then "1" else if x=='L' then "0" else "2") <$> show <$> binOrdList m in putStr $ "Numbers from 0 up to (but not including) " ++ (show $ elems m) ++ " in binary:\n" ++ unlines l
